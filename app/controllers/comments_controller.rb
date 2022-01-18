07d5547d49f8298b1988279006a341a53fda5b03
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-01-18 23:12:12 +0100

    add comment create endpont

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,17 +4,24 @@ class CommentsController < ApplicationController
     @comments = Post.find(params[:post_id]).comments
     json_response(@comments)
   end
+
   def create
     @comment = Comment.new(comment_params)
     @comment.post_id = params[:post_id]
     @comment.user = current_user
 
-    if @comment.save
-      redirect_to posts_path, notice: 'Comment was successfully created.'
-    else
-      redirect_to posts_path, alert: @comment.errors.full_messages.join('. ').to_s
+    iif @comment.save
+    respond_to do |format|
+      format.html { redirect_to posts_path, notice: 'Comment was successfully created.' }
+      format.json { render json: { message: 'Comment Created!' } }
+    end
+  else
+    respond_to do |format|
+      format.html { redirect_to posts_path, alert: @comment.errors.full_messages.join('. ').to_s }
+      format.json { render json: { message: 'Error!' } }
     end
   end
+
 
   private
 
--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -2,4 +2,11 @@ module Response
     def json_response(object, status = :ok)
       render json: object, status: status
     end
+
+    def multi_response(object)
+      respond_to do |format|
+        format.html
+        format.json { render json: object }
+      end
+    end
   end
