45e7f0fde459d3338d761ac137b07e4185b63fed
JuliCarracedo <julicarra@hotmail.com>
2022-01-18 17:43:42 -0300

    Add all posts endpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,10 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @post = Post.new
+    # @post = Post.new
+    @posts = Post.all
     timeline_posts
+    json_response(@posts)
   end
 
   def create
