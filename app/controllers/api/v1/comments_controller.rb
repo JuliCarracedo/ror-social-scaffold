5ecd42bd522b046839765165cb8c5ce9dc8037be
JuliCarracedo <julicarra@hotmail.com>
2022-01-19 16:44:50 -0300

    add api controllers

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,33 +1,37 @@
-class CommentsController < ApplicationController
+module Api 
+  module V1
+  class CommentsController < ApplicationController
 
-    def index
-      @comments = Post.find(params[:post_id]).comments
-      json_response(@comments)
-    end
-  
-    def create
-      @comment = Comment.new(comment_params)
-      @comment.post_id = params[:post_id]
-      @comment.user = current_user
-  
-      if @comment.save
-  
-        multi_response(message: 'Comment Created')
-   
-      else
-      respond_to do |format|
-     
-        multi_response(message: "Couldn't create comment", status: :error)
-        
+      def index
+        @comments = Post.find(params[:post_id]).comments
+        json_response(@comments)
+      end
+    
+      def create
+        @comment = Comment.new(comment_params)
+        @comment.post_id = params[:post_id]
+        @comment.user = current_user
+    
+        if @comment.save
+    
+          multi_response(message: 'Comment Created')
+    
+        else
+        respond_to do |format|
+      
+          multi_response(message: "Couldn't create comment", status: :error)
+          
+          end
         end
       end
-    end
   
   
-    private
-  
-    def comment_params
-      params.require(:comment).permit(:content)
+      private
+    
+      def comment_params
+        params.require(:comment).permit(:content)
+      end
     end
   end
+end
   
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,24 +1,28 @@
-class PostsController < ApplicationController
-    before_action :authenticate_user!
-  
-    def index
-      @posts = Post.all
-      json_response(@posts)
-    end
-  
-    def create
-      @post = current_user.posts.new(post_params)
-      if @post.save()
-        json_response({message: 'Post was created'})
-      else
-        json_response({message: "Post couldn't be created"}, :error)
+module Api
+  module V1
+
+  class PostsController < ApplicationController
+      before_action :authenticate_user!
+    
+      def index
+        @posts = Post.all
+        json_response(@posts)
+      end
+    
+      def create
+        @post = current_user.posts.new(post_params)
+        if @post.save()
+          json_response({message: 'Post was created'})
+        else
+          json_response({message: "Post couldn't be created"}, :error)
+        end
+      end
+    
+      private
+      
+      def post_params
+        params.require(:post).permit(:content)
       end
     end
-  
-    private
-    
-    def post_params
-      params.require(:post).permit(:content)
-    end
   end
-  
+end
