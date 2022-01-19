aeb3168b7beaa82cef1b3612153267b6c72a50b6
JuliCarracedo <julicarra@hotmail.com>
2022-01-19 18:55:13 -0300

    add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
-  devise_for :users
-
+  devise_for :users,
+             controllers: {
+                 sessions: 'users/sessions',
+                 registrations: 'users/registrations'
+             }
+  get '/member-data', to: 'members#show'
+  
   namespace :api do
     devise_scope :user do
       post 'login', to: '/users/sessions#create', defaults:{ format: :json}
