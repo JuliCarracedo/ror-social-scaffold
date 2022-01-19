b0793ee96a4e7836be8ea00d6715d48c2909d84f
JuliCarracedo <julicarra@hotmail.com>
2022-01-19 19:14:07 -0300

    add members controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,23 +1,31 @@
 class Users::SessionsController < Devise::SessionsController
-  respond_to :json
-
-  private
-
-  def respond_with(_resource, _opts = {})
-    render json: { message: 'You are logged in.' }, status: :ok
+  def create
+    respond_to do |format|
+      format.html { super }
+      format.json do
+        warden.authenticate!(scope: resource_name, recall: "#{controller_path}#new")
+        render status: 200, json: { message: 'Log in successful' }
+      end
+    end
   end
 
-  def respond_to_on_destroy
-    log_out_success && return if current_user
+  # private
 
-    log_out_failure
-  end
+  # def respond_with(_resource, _opts = {})
+  #   render json: { message: 'You are logged in.' }, status: :ok
+  # end
 
-  def log_out_success
-    render json: { message: 'You are logged out.' }, status: :ok
-  end
+  # def respond_to_on_destroy
+  #   log_out_success && return if current_user
 
-  def log_out_failure
-    render json: { message: 'Hmm nothing happened.' }, status: :unauthorized
-  end
+  #   log_out_failure
+  # end
+
+  # def log_out_success
+  #   render json: { message: 'You are logged out.' }, status: :ok
+  # end
+
+  # def log_out_failure
+  #   render json: { message: 'Hmm nothing happened.' }, status: :unauthorized
+  # end
 end
